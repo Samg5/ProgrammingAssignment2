bc073633b45919bc1cafa5eb5488b0563e840834
Sam G <sshaa54@gmail.com>
2014-12-21 16:05:52 -0500

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 ## ================================================================================
 ## Function to create an R Object.
 ## 1. Define a variable ‘x' 
@@ -10,38 +9,33 @@
 ## 4. Function getImatrix() to get the cached inverse matrix.
 ## ================================================================================
 
-makeCacheMatrix <- function(x = matrix()) {
-	x <- NULL
-    		get <- function() m
-    		setImatrix <- function(Imatrix) x <<- Imatrix
-    		getImatrix <- function() x
-    #List of functions 
-    		list(get=get, setImatrix=setImatrix, getImatrix=getImatrix)
+makeCacheMatrix <- function(m=matrix()) {
+    x <- NULL
+    get <- function() m
+    setImatrix <- function(Imatrix) x <<- Imatrix
+    getImatrix <- function() x
+
+    # return a list of functions as an R object
+    list(get=get, setImatrix=setImatrix, getImatrix=getImatrix)
 }
 
-## Write a short comment describing this function
 ## ================================================================================
 ## Function to actually inverse the matrix m. 
 ## This will check if inverse matrix is cached, if not then it calculates the inverse and return the result.
 ## ================================================================================
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-	x <- m$getImatrix()
-    
-		if(!is.null(x))
-			{
-        		message(“Matrix is Cached. Printing matrix....")
-        			return(x)
-    			}
-    		else 	{
-        
-	message(“Matrix not cached. Inversing Matrix...”)
-        		data <- m$get() # obtains matrix from object x
-        		x <- solve(data) # finds inverse matrix
-        			m$setImatrix(x) # assigns resulting inverse matrix to object x
-        				message("Done.")
-        	return(x)
-    			}			
-
+cacheSolve <- function(m) {
+    x <- m$getImatrix()
+    if(!is.null(x)){
+        message("Matrix is Cached. Printing matrix....")
+        return(x)
+    }
+    else {
+        message("Matrix not cached. Inversing Matrix...")
+        data <- m$get() # obtains matrix from object x
+        x <- solve(data) # finds inverse matrix
+        m$setImatrix(x) # assigns resulting inverse matrix to object x
+        message("Done.")
+        return(x)
+    }
 }
